a8ab5147e0a35b30b91833d944f44cca8d0306d8
Aron Nemeth <nemeth.aron33@gmail.com>
2025-12-01 17:59:22 +0100

    update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import { getWeatherForCity } from "./weatherService.js";
 import { getOutfitSuggestion } from "./fitLogic.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
